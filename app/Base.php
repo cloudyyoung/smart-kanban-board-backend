ea71ee53b744a5cda24049effd18fcec1e005842
Cloudy Young <cloudyyoung74@outlook.com>
2020-02-05 16:02:49 -0700

    Init Commit

---
+++ app/Base.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App;
+
+use Flight;
+use Throwable;
+
+class Base
+{
+
+    public static function set($attribute, $value)
+    {
+        $this->$attribute = $value;
+        return $this;
+    }
+
+    public static function get($attribute)
+    {
+        return $this->$attribute;
+    }
+
+    public static function read()
+    {
+        Flight::ret(200, "OK", $this);
+    }
+
+    # Run a function in the class
+    public static function run($funcName, $funcName2)
+    {
+        try {
+            if ($funcName == null) {
+                $funcName = 'read';
+            }
+            if (substr($funcName, 0, 1) == "_" || substr($funcName, 0, 3) == "get" || substr($funcName, 0, 3) == "set" || substr($funcName, 0, 3) == "run") {
+                throw new Error('denied');
+            }
+
+            if ($funcName2 != null) {
+                $this->$funcName . '_' . $funcName2();
+            } else {
+                $this->$funcName();
+            }
+        } catch (Throwable $e) {
+            Flight::ret(405, "method not allowed");
+        }
+    }
+
+    public static function view($viewName, $viewName2 = null)
+    {
+        try {
+            if ($viewName == null) {
+                $viewName = 'read';
+            }
+            if (substr($viewName, 0, 1) == "_") {
+                throw new Throwable('denied');
+            }
+
+            if ($viewName2 != null) {
+                Flight::render(ucwords($viewName) . ucwords($viewName2), array('user' => Flight::get('user')), 'body');
+            } else {
+                Flight::render(ucwords($viewName), array('user' => Flight::get('user')), 'body');
+            }
+
+            Flight::render('_layout');
+        } catch (Throwable $e) {
+            throw $e;
+        }
+    }
+}
---
+++ index.php
@@ -0,0 +1,77 @@
+<?php
+
+define('CONFIG', parse_ini_file('config.ini', true, INI_SCANNER_TYPED));
+define('ROOT', __DIR__);
+
+session_start();
+
+require 'vendor/autoload.php';
+
+Flight::set('flight.log_errors', true);
+Flight::set('flight.views.path', __DIR__ . '/view');
+
+
+if (CONFIG['app']['debug']) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+}
+
+
+Flight::map('error', function (Throwable $e) {
+    // Handle error
+    Flight::ret(500, "Internal Server Error");
+    if (CONFIG['app']['debug']) {
+        echo $e;
+        echo $e->getTraceAsString();
+    }
+});
+
+Flight::map('notFound', function () {
+    // Handle not found
+    Flight::get('user')->view('404');
+});
+
+
+
+Flight::register('db', 'mysqli', array(CONFIG['database']['host'], CONFIG['database']['username'], CONFIG['database']['password'], CONFIG['database']['database']), function ($db) {
+    $db->set_charset("utf8");
+});
+
+Flight::map('sql', function ($sql, $fetch_all = false) {
+    $db = Flight::db();
+    $res = $db->query($sql);
+    if (is_bool($res)) {
+        return $res;
+    }
+
+    if ($fetch_all) {
+        $ret = [];
+        while ($row = $res->fetch_assoc()) {
+            $ret[] = (object) $row;
+        }
+        return $ret;
+    }
+    return (object) $res->fetch_assoc();
+});
+
+// RESTful
+Flight::map('ret', function ($code = 204, $message = '', $array = null) {
+    $message = ucwords($message);
+    header("HTTP/1.1 $code $message");
+    if (!empty($array)) {
+        Flight::json($array);
+    }
+    Flight::stop();
+});
+
+
+
+
+Flight::route('/', function () {
+    echo 'Kanban whah???';
+});
+
+
+
+Flight::start();
